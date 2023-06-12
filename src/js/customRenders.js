c8b51cbad546f3fac4cac76ba693dd9d62dcb5fd
debbieacciona <deperalta.ext@acciona.com>
2023-06-12 15:11:19 +0200

    Table with add delete and optimized

---
+++ src/js/customRenders.js
@@ -0,0 +1,47 @@
+const addClassWhenNeeded = (td, cellProperties) => {
+    const className = cellProperties.className;
+
+    if (className !== void 0) {
+        Handsontable.dom.addClass(td, className);
+    }
+};
+
+window.progressBarRenderer = function progressBarRenderer(
+    instance,
+    td,
+    row,
+    column,
+    prop,
+    value,
+    cellProperties
+) {
+    const div = document.createElement("div");
+
+    div.style.width = `${value * 10}px`;
+
+    addClassWhenNeeded(td, cellProperties);
+    Handsontable.dom.addClass(div, "progressBar");
+    Handsontable.dom.empty(td);
+
+    td.appendChild(div);
+}
+
+window.starRenderer = function starRenderer(
+    instance,
+    td,
+    row,
+    column,
+    prop,
+    value,
+    cellProperties
+) {
+    Handsontable.renderers.TextRenderer.apply(this, [
+        instance,
+        td,
+        row,
+        column,
+        prop,
+        "★".repeat(value),
+        cellProperties
+    ]);
+}
